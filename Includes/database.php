1eb2985e039a2b40282d49d836ae0a7baf1090ed
Alger Makiputin <algerzxc@gmail.com>
2017-08-23 07:19:53 +0800

    remove expose info

--- Includes/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('server','127.0.0.1');
-define('username','homestead');
-define('password','secret');
-define('dbname','classroom_management_system');
-
-$con = mysqli_connect(server,username,password,dbname);
